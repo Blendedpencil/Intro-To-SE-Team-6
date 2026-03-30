83a288efcbbf6ebce8e6a029c3defe1eff6f134f
Dean <deanpatt07@gmail.com>
2026-03-30 11:38:28 -0500

    yml Test 4
    Testing with example tests (#267)

--- backend/adminpanel/tests.py
+++ backend/adminpanel/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+class testCase(TestCase):
+    def setUp(self):
+        testVar = 5
+    def testThing(self):
+        self.assertEqual(5, 5)
+
+class testCase2(TestCase):
+    def setUp(self):
+        testVar = 5
+    def testThing(self):
+        self.assertEqual(5, 5)
